fb05adc2723b60f9e55520b1bcae3c87746226ad
mathSusan <sbmck@yahoo.com>
2019-03-11 19:23:05 -0500

    updating to the latest Angular and npm

--- src/app/app-bootstrap/app-bootstrap.module.ts
+++ src/app/app-bootstrap/app-bootstrap.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule } from 'ngx-bootstrap';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
